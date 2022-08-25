2cbed716b56c99f6186e2c6bbf6c5092cbde11ad
Mercy <mercycheruiyot2013@gmail.com>
2022-08-25 14:26:12 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide =(parameter1,parameter2) =>parameter1 / parameter2;// arrow function for division
+divide(2000,100);
+const square=(parameter1)=>parameter1*parameter1;
+square(20);//arrow function for multiplication
+const add=(parameter1,parameter2)=>parameter1+parameter2;
+add(2000,100);//function for addition
